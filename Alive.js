8d695e85527d4edaa8dd0a44204bd08b2cc5ef8a
chris-av225 <anosv676@gmail.com>
2025-06-12 09:22:20 +0000

    Update Alive.js

--- Alive.js
+++ Alive.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import axios from 'axios';
 
-const url = 'https://flash-md-v2-3.onrender.com';
+const url = 'https://flash-md-v2-4.onrender.com';
 
 cron.schedule('*/14 * * * *', async () => {
   try {
